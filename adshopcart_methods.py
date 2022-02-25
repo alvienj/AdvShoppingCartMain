df519807751cf686a964a7d9d9b8148871d157ca
alvienj <99428780+alvienj@users.noreply.github.com>
2022-02-25 10:51:55 -0800

    Update adshopcart_methods.py

--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -6,18 +6,23 @@ import datetime
 driver = webdriver.Chrome('C:/Users/User/PycharmProjects/si/advantage_shopping_cart/chromedriver.exe')
 
 def setUp():
+    if driver is not None:
+        print(f'Test started at: {datetime.datetime.now()}')
+        print(f'--------------------------------------')
+
+
     # Make a full screen
     driver.maximize_window()
 
     # Let's wait for the browser response in general
     driver.implicitly_wait(30)
 
-    # Navigating to the Moodle app website
+    # Navigating to the Advantage Shopping website
     driver.get(locators.adshopcart_url)
 
 
     # Checking that we're on the correct URL address and we're seeing correct title
-    if driver.current_url == locators.adshopcart_url and driver.title == 'Advantage Shopping':
+    if driver.current_url == locators.adshopcart_url:
         print(f'We\'re at the Advantage Shopping Cart homepage -- {driver.title}')
         print(f'We\'re seeing title message -- "Advantage Shopping"')
     else:
