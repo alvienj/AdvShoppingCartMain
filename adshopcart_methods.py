33fa26ba6fb8bc1f95a50bccd50babc97324b535
alvienj <99428780+alvienj@users.noreply.github.com>
2022-02-27 22:12:08 -0800

    Update adshopcart_methods.py

--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -2,14 +2,13 @@ import adshopcart_locators as locators
 from selenium import webdriver
 import datetime
 
-
 driver = webdriver.Chrome('C:/Users/User/PycharmProjects/si/advantage_shopping_cart/chromedriver.exe')
+
 
 def setUp():
     if driver is not None:
         print(f'Test started at: {datetime.datetime.now()}')
         print(f'--------------------------------------')
-
 
     # Make a full screen
     driver.maximize_window()
@@ -19,17 +18,21 @@ def setUp():
 
     # Navigating to the Advantage Shopping website
     driver.get(locators.adshopcart_url)
-    
-    url_title = driver.title
+
+
 
     # Checking that we're on the correct URL address and we're seeing correct title
-    if driver.current_url == locators.adshopcart_url and url_title == driver.title:
+    if driver.current_url == locators.adshopcart_url:
         print(f'We\'re at the Advantage Shopping homepage -- {driver.title}')
-        print(f'We\'re seeing title message -- "Advantage Shopping"')
     else:
         print(f'We\'re not at the Advantage Shopping homepage. Check your code!')
-        driver.close()
-        driver.quit()
+    try:
+        assert 'Advantage Shopping' in driver.title
+        print(f'Title is good!')
+    except Exception as e:
+        print(f'Title is not matching, check code please')
+
+
 
 def tearDown():
     if driver is not None:
@@ -37,9 +40,3 @@ def tearDown():
         print(f'Test Completed at: {datetime.datetime.now()}')
         driver.close()
         driver.quit()
-
-
-
-
-setUp()
-tearDown()
