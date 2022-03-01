063ffbf827e0bf14b66d05f90a46dcfc9281d937
alvienj <99428780+alvienj@users.noreply.github.com>
2022-02-28 22:37:10 -0800

    Update adshopcart_tests.py

--- adshopcart_tests.py
+++ adshopcart_tests.py
@@ -1,5 +1,14 @@
 import adshopcart_methods as methods
 import adshopcart_locators as locators
 
+
 setUp()
+createnewaccount()
+myaccount()
+shoppingcart()
+signout()
+signin()
+deleteuser()
+checkifuserisdeleted()
+logger()
 tearDown()
