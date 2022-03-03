7f614c2b465d16e46c9d128a4fbeda3adfc888e0
alvienj <99428780+alvienj@users.noreply.github.com>
2022-03-03 09:31:25 -0800

    Update adshopcart_tests.py

--- adshopcart_tests.py
+++ adshopcart_tests.py
@@ -1,17 +1,21 @@
+import unittest
 import adshopcart_methods as methods
 import adshopcart_locators as locators
 
-
-setUp()
-createnewaccount()
-myaccount()
-shoppingcart()
-signout()
-signin()
-deleteuser()
-checkifuserisdeleted()
-displays()
-tabs()
-contactus()
-tearDown()
-logger()
+class MoodleAppPositiveTestCases(unittest.TestCase):
+    
+    @staticmethod 
+    def advantageshopping():
+        setUp()
+        createnewaccount()
+        myaccount()
+        shoppingcart()
+        signout()
+        signin()
+        deleteuser()
+        checkifuserisdeleted()
+        displays()
+        tabs()
+        contactus()
+        tearDown()
+        logger()
